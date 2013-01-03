b98c21c2a9102c10953d95e54777304bcec07e7f
logical42 <han@thirstlabs.com>
2013-01-03 11:52:47 -0800

    scaffold template

--- lib/generators/hanber/scaffold_generator.rb
+++ lib/generators/hanber/scaffold_generator.rb
@@ -23,21 +23,21 @@ module Hanber
 
         insert_into_file "app/assets/javascripts/router.js", after: ".map(function(match) {\n" do
           %Q(  
-            match("/#{file_name.pluralize}").to("#{file_name.pluralize}", function(match) {
-              match("/").to("#{file_name.pluralize}Index");
-              match("/new").to("#{file_name}New");
-              match("/:#{file_name.underscore}_id").to("#{file_name}", function(match) {
-                match('/').to("#{file_name}Show")
-                match("/content").to("#{file_name}Edit");
-              });
-            });
+  match("/#{file_name.pluralize}").to("#{file_name.pluralize}", function(match) {
+    match("/").to("#{file_name.pluralize}Index");
+    match("/new").to("#{file_name}New");
+    match("/:#{file_name.underscore}_id").to("#{file_name}", function(match) {
+      match('/').to("#{file_name}Show")
+      match("/content").to("#{file_name}Edit");
+    });
+  });
           )
         end
       end
       def create_route_handlers
         resource_hash = {show: "#{file_name}Show", index: "#{file_name.pluralize}Index", new: "#{file_name}New", edit: "#{file_name}Edit"}
         resource_hash.each do |restful_action, restful_name|
-          template 'route_handler.js', File.join("app/assets/javascripts/route_handlers/#{file_name}_routes", class_path, "#{restful_name.underscore}.js")
+          template('scaffolded_route.js', File.join("app/assets/javascripts/route_handlers/#{file_name}_routes", class_path, "#{restful_name.underscore}.js"),  :assigns => { :rest_action => restful_name })
         end
       end
     end
